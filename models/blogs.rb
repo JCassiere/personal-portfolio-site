95ebe7c44b06b080d3b9dcc71885287ee3c3ff34
Joe Cassiere <jcassiere91@gmail.com>
2017-08-13 19:37:13 -0400

    more formatting

--- models/blogs.rb
+++ models/blogs.rb
@@ -1,15 +1,16 @@
 require 'io/console'
 
 class Blog
-	attr_reader :posts
+  attr_reader :posts
 
-	def initialize(blog_dir)
-		@posts = []
-		blogs = (Dir.entries(blog_dir).sort_by {|file| File.mtime(blog_dir + file)}).reverse
-		blogs.each do |file|
-		  unless (file == '.' or file == '..')
-		  	@posts << IO.read(blog_dir + file)
-			end
-		end
-	end
+
+  def initialize(blog_dir)
+    @posts = []
+    blogs = (Dir.entries(blog_dir).sort_by {|file| File.mtime(blog_dir + file)}).reverse
+    blogs.each do |file|
+      unless (file == '.' or file == '..')
+        @posts << IO.read(blog_dir + file)
+      end
+    end
+  end
 end
