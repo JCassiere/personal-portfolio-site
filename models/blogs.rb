3d1c936f445acad5c77267e23b5dfbe07c6a6999
Joe Cassiere <jcassiere91@gmail.com>
2017-08-13 20:02:01 -0400

    added config.ru

--- models/blogs.rb
+++ models/blogs.rb
@@ -3,7 +3,6 @@ require 'io/console'
 class Blog
   attr_reader :posts
 
-
   def initialize(blog_dir)
     @posts = []
     blogs = (Dir.entries(blog_dir).sort_by {|file| File.mtime(blog_dir + file)}).reverse
